b9cf5451be49faacfd325ad2a9b05251900301b8
David Guzman <davidguzman.sv@hotmail.com>
2023-10-14 21:02:28 -0600

    compras

---
+++ js/compras.js
@@ -0,0 +1,20 @@
+
+const iniciar = function () {
+    const comprar = document.getElementById("idBtnComprar");
+    if (comprar.addEventListener) {
+        comprar.addEventListener(
+            "click", 
+            function () {
+                location.href = "tarjeta.html";
+            },
+            false
+        );
+    }   else if (comprar.attachEvent) {
+        comprar.attachEvent("onclick", function () {
+            location.href = "tarjeta.html";
+    });
+    }
+}
+
+///inicializando datos
+iniciar();
