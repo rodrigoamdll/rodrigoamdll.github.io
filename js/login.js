728377f52b3d32b57ef885e0f981fe5d2e410d49
Rodrigo Aguilera <rodrigoaguilerad2000@gmail.com>
2023-10-14 19:37:20 -0600

    login added

---
+++ js/login.js
@@ -0,0 +1,5 @@
+// Contenido de script.js
+document.getElementById("iniciarSesion").addEventListener("click", function() {
+    // Redirige a la página de destino
+    window.location.href = ""; // Reemplaza con la URL de tu página de destino
+});
