804dc5e7df805f7ae164236d8394845abaa894cb
Rodrigo Aguilera <rodrigoaguilerad2000@gmail.com>
2023-11-11 11:33:00 -0600

    estrenos updated

--- js/script.js
+++ js/script.js
@@ -1,29 +1,37 @@
-var swiper = new Swiper(".slide-content", {
-    slidesPerView: 3,
-    spaceBetween: 25,
-    loop: true,
-    centerSlide: 'true',
-    fade: 'true',
-    grabCursor: 'true',
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  centerSlide: true, 
+  fade: true, 
+  grabCursor: true, 
+  autoplay: {
+    delay: 5000, 
+  },
 
-    breakpoints:{
-        0: {
-            slidesPerView: 1,
-        },
-        520: {
-            slidesPerView: 2,
-        },
-        950: {
-            slidesPerView: 3,
-        },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true, 
+    dynamicBullets: true,   
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
     },
-  });
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+});
